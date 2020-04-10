128189f4a5f5836dc59c80e7657d70aed2c12822
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2020-04-10 13:31:01 -0400

    chore: add mongoose

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 import Hapi from '@hapi/hapi';
+import Joi from '@hapi/joi';
+import Mongoose from 'mongoose';
+
 
 const init = async () => {
   const server = new Hapi.Server({
     port: 3000,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+  });
+
+  Mongoose.connect('mongodb://localhost/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   });
 
   await server.start();
